a3eb0b63221ba7911bac506863db277f21cd5d80
Khaled Abdulhalim <lga.sig.khaled@gmail.com>
2020-10-21 09:08:15 +0200

    verlan  function

--- exercice-1/String.js
+++ exercice-1/String.js
@@ -20,6 +20,26 @@
   }
 
 
+  const verlan = (str) => {
+   let words = []
+   words = str.match(/\S+/g);
+   let result = ""
+   for (var i = 0; i < words.length; i++) {
+      result += words[i].split('').reverse().join('') + " ";
+   }
+   return result
+
+  }
+
+  
+ console.log(verlan("Hello World"))
+
+
+
+
+
+
+
 
 
 
